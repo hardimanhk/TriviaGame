61b5ff9ee8a5f56aacf4af44e2ad00c395f6590f
hilbo <hardimanhk@gmail.com>
2018-08-31 14:04:01 -0400

    question clicks work, need to implement a timer

--- javascript/game2.js
+++ javascript/game2.js
@@ -41,6 +41,7 @@ var randomizedAnswerSet = [];
 var correct = 0;
 var incorrect = 0;
 var startInterval;
+var delay;
 
 // create a function to arrange the answers randomly but not repeat answers
 function setRandomAnswers() {
@@ -54,6 +55,10 @@ function setRandomAnswers() {
 
 // function to display a question and its answers
 function displayQuestion() {
+    $("#win").text("YOU'RE RIGHT WOO!!!");
+    $("#win").hide();
+    $("#lose").text("WRONG!");
+    $("#lose").hide();
     setRandomAnswers();
     $("#question").text(questions[count].q);
     $("#ans1").text(questions[count][randomizedAnswerSet[0]]);
@@ -66,15 +71,43 @@ function displayQuestion() {
     $("#ans4").attr("value", randomizedAnswerSet[3]);
     count++;
     if (count === questions.length) {
+        // run endgame 
         count = 0;
     }
 }
 
 // create a function that will cycle through questions
 function questionCycle() {
+    $("#game").show();
+    $("#start").text("Start Over");
     displayQuestion();
-    setInterval(displayQuestion, 5000);   
+    startInterval = setInterval(displayQuestion, 5000);   
 }
+
+// need to show a timer
+// if timer runs out show time ran out screen - after two seconds continue game
+$(".answerButton").click(function() {
+    // if user clicks correct answer show win screen - after two seconds continue game
+    if ($(this).attr("value") === "correct") {
+        correct++;
+        clearInterval(startInterval);
+        $("#game").hide();
+        $("#win").show();
+        delay = setTimeout(questionCycle, 2000);
+    }
+    // if user chooses incorrectly show show inccorect guess screen - after two seconds contnue game
+    else {
+        incorrect++;
+        clearInterval(startInterval);
+        $("#game").hide();
+        $("#lose").show();
+        delay = setTimeout(questionCycle, 2000);
+    }
+
+})
+
+
+// if count === questions.length show end screen with score
 
 // create a function that will be passed in once "start game" is clicked
 $("#start").click(questionCycle);
